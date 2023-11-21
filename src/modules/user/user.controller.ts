231809ec23277d1538b3b63d5235a85985bd3cb5
GaoZeWen <1440651163@qq.com>
2023-11-21 20:24:35 +0800

    feat: 实现 User 的删、改、查 demo 接口

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { UserService } from './user.service';
+import { User } from './models/user.entity';
 
 @Controller()
 export class UserController {
@@ -14,5 +15,31 @@ export class UserController {
       desc: '管理员',
       tel: '8800088',
     });
+  }
+
+  @Get('/delete')
+  async delete(): Promise<boolean> {
+    return await this.userService.delete(
+      '22c32f08-02a9-48cf-a5ba-9fd581fde599',
+    );
+  }
+
+  @Get('/update')
+  async update(): Promise<boolean> {
+    return await this.userService.update(
+      'b1e2e948-f775-4aef-be86-c29a9f75835e',
+      {
+        account: 'admin',
+        password: '123456',
+        name: '天空教育管理员111',
+        desc: '管理员',
+        tel: '8800088',
+      },
+    );
+  }
+
+  @Get('/find')
+  async find(): Promise<User> {
+    return await this.userService.find('b1e2e948-f775-4aef-be86-c29a9f75835e');
   }
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -13,6 +13,24 @@ export class UserService {
   // 新增一个用户
   async create(entity: DeepPartial<User>): Promise<boolean> {
     const res = await this.userRepository.insert(entity);
-    return res && res.raw.affectedRows > 0;
+    return res && res.raw && res.raw.affectedRows > 0;
+  }
+
+  // 删除用户
+  async delete(id: string): Promise<boolean> {
+    const res = await this.userRepository.delete(id);
+    return res && res.affected > 0;
+  }
+
+  // 修改用户
+  async update(id: string, entities: DeepPartial<User>): Promise<boolean> {
+    const res = await this.userRepository.update(id, entities);
+    return res && res.affected > 0;
+  }
+
+  // 查询用户
+  async find(id: string): Promise<User> {
+    const res = await this.userRepository.findOne({ where: { id } });
+    return res;
   }
 }
