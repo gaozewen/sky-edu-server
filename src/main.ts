e733832efbe4dc62adc933fed946a48b0cf1ef48
GaoZeWen <1440651163@qq.com>
2023-11-22 16:58:53 +0800

    feat: 解决跨域问题

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:5174',
+      'https://sky-edu-api.gaozewen.com/graphql',
+    ],
+    // 仅支持 graphql 的 post 请求
+    methods: ['POST'],
+  });
   await app.listen(3000);
 }
 bootstrap();
