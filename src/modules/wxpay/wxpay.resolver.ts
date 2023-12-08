8a8aa0fe39027bbb27e8776e162c4fd02116c4ec
GaoZeWen <1440651163@qq.com>
2023-12-08 22:19:27 +0800

    feat: 使用 IS_MOCK_PAY 控制模拟支付

--- src/modules/wxpay/wxpay.resolver.ts
+++ src/modules/wxpay/wxpay.resolver.ts
@@ -4,6 +4,7 @@ import { WECHAT_PAY_MANAGER } from 'nest-wechatpay-node-v3';
 import { v4 } from 'uuid';
 import WxPay from 'wechatpay-node-v3';
 
+import { IS_MOCK_PAY } from '@/common/constants';
 import {
   PRODUCT_NOT_EXIST,
   SUCCESS,
@@ -71,7 +72,21 @@ export class WxPayResolver {
       // },
     };
 
-    const result = await this.wxPay.transactions_jsapi(params);
+    let result;
+    if (IS_MOCK_PAY) {
+      result = {
+        appId: 'wx2421b1c4370ec43b', //公众号ID，由商户传入
+        timeStamp: '1395712654', //时间戳，自1970年以来的秒数
+        nonceStr: 'e61463f8efa94090b1f366cccfbbb444', //随机串
+        package: 'prepay_id=up_wx21201855730335ac86f8c43d1889123400',
+        signType: 'RSA', //微信签名方式：
+        //微信签名
+        paySign:
+          'oR9d8PuhnIc+YZ8cBHFCwfgpaK9gd7vaRvkYD7rthRAZ/X+QBhcCYL21N7cHCTUxbQ+EAt6Uy+lwSN22f5YZvI45MLko8Pfso0jm46v5hqcVwrk6uddkGuT+Cdvu4WBqDzaDjnNa5UK3GfE1Wfl2gHxIIY5lLdUgWFts17D4WuolLLkiFZV+JSHMvH7eaLdT9N5GBovBwu5yYKUR7skR8Fu+LozcSqQixnlEZUfyE55feLOQTUYzLmR9pNtPbPsu6WVhbNHMS3Ss2+AehHvz+n64GDmXxbX++IOBvm2olHu3PsOUGRwhudhVf7UcGcunXt8cqNjKNqZLhLw4jq/xDg==',
+      };
+    } else {
+      result = await this.wxPay.transactions_jsapi(params);
+    }
     return {
       code: SUCCESS,
       data: result as WxPayConfigVO,
