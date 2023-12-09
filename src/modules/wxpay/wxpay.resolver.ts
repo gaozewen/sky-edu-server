3fb715a70ebd85c42dbca0d2fb428f5b83ec8b60
GaoZeWen <1440651163@qq.com>
2023-12-09 10:02:20 +0800

    feat: 完成模拟微信支付接口

--- src/modules/wxpay/wxpay.resolver.ts
+++ src/modules/wxpay/wxpay.resolver.ts
@@ -1,5 +1,5 @@
 import { Inject, UseGuards } from '@nestjs/common';
-import { Args, Int, Query, Resolver } from '@nestjs/graphql';
+import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { WECHAT_PAY_MANAGER } from 'nest-wechatpay-node-v3';
 import { v4 } from 'uuid';
 import WxPay from 'wechatpay-node-v3';
@@ -11,12 +11,14 @@ import {
   WX_OPENID_NOT_EXIST,
 } from '@/common/constants/code';
 import { JwtUserId } from '@/common/decorators/JwtUserId.decorator';
+import { ResultVO } from '@/common/vo/result.vo';
 
 import { JwtGqlAuthGuard } from '../auth/guard/jwt.gql.guard';
 import { OrderStatus } from '../order/models/order.entity';
 import { OrderService } from '../order/order.service';
 import { ProductService } from '../product/product.service';
 import { StudentService } from '../student/student.service';
+import { TradeType } from '../wx-order/models/wx-order.entity';
 import { WxPayConfigResultVO, WxPayConfigVO } from './vo/wxpay.vo';
 
 @Resolver()
@@ -115,6 +117,60 @@ export class WxPayResolver {
       code: SUCCESS,
       data: result as WxPayConfigVO,
       message: '获取微信支付配置信息成功',
+    };
+  }
+
+  @Mutation(() => ResultVO)
+  async mockWxPay(
+    @JwtUserId() id: string,
+    @Args('productId') productId: string,
+    @Args('quantity', { type: () => Int }) quantity: number, // 购买的商品数量
+    @Args('amount', { type: () => Int }) amount: number, // 以分为单位
+  ): Promise<ResultVO> {
+    const student = await this.studentService.findById(id);
+    const product = await this.productService.findById(productId);
+    const outTradeNo = v4().replace(/-/g, '');
+    // 1.创建项目自己的预支付订单
+    await this.orderService.create({
+      outTradeNo,
+      tel: student.tel,
+      quantity,
+      amount,
+      status: OrderStatus.USERPAYING,
+      product: {
+        id: productId,
+      },
+      store: {
+        id: product.store.id,
+      },
+      student: {
+        id,
+      },
+      wxOrder: {
+        appid: 'wx1234567890123',
+        mchid: '123456789',
+        out_trade_no: outTradeNo,
+        transaction_id: `transaction${outTradeNo}`,
+        trade_type: TradeType.JSAPI,
+        trade_state: OrderStatus.SUCCESS,
+        trade_state_desc: '支付成功',
+        bank_type: 'OTHERS',
+        attach: '',
+        success_time: '2023-12-09T00:48:25+08:00',
+        openid: 'openidopenidopenidopenidopenid',
+        total: amount,
+        payer_total: amount,
+        currency: 'CNY',
+        payer_currency: 'CNY',
+        store: {
+          id: product.store.id,
+        },
+      },
+    });
+
+    return {
+      code: SUCCESS,
+      message: '支付成功',
     };
   }
 }
