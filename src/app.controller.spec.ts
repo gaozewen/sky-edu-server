5517ad111985882fcee983650b88da98adf48f64
GaoZeWen <1440651163@qq.com>
2023-12-13 12:48:51 +0800

    feat: 集成 sqlite，用作数据库单元测试

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,6 +9,14 @@ describe('AppController', () => {
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot({
+          type: 'sqlite',
+          database: ':memory:',
+          synchronize: true,
+          autoLoadEntities: true,
+        }),
+      ],
       controllers: [AppController],
       providers: [AppService],
     }).compile();
@@ -19,5 +28,11 @@ describe('AppController', () => {
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });
+
+    // it('数据库测试', async () => {
+    //   await appController.create('1');
+    //   const res = await appController.find('1')
+    //   expect(res.nickname).toBe('1');
+    // });
   });
 });
